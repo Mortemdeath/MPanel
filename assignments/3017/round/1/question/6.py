4859a335fff6b27c063d853a723604f18229b2ad
Mortem <115175467+Mortemdeath@users.noreply.github.com>
2025-09-24 20:03:38 +0100

    Create 6.py

---
+++ assignments/3017/round/1/question/6.py
@@ -0,0 +1,9 @@
+calculateDiscount("price", "percentage"):
+  discount = price * (percentage / 100)
+return price - discount
+
+oriqinalPrice = int(input("Enter a price"))
+
+calculateDiscount(originalPrice, 15))
+print(calculateDiscount(originalPrice, 20))
+print(calculateDiscount(-20, 15))
