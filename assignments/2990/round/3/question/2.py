0f968f86eea712c0abe892df311b7e0f486e30ed
Mortem <115175467+Mortemdeath@users.noreply.github.com>
2025-09-24 19:57:46 +0100

    Create 2.py

---
+++ assignments/2990/round/3/question/2.py
@@ -0,0 +1,13 @@
+egg=int(input(""))
+for i in range(0,egg):
+  w=int(input(""))
+  if w>27:
+    print("Extra Large")
+  elif w>24:
+    print("Large")
+  elif w>21:
+    print("Medium")
+  else:
+    print("Small")
+while 0>0:
+  print(1)
